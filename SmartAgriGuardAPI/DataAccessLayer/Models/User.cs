b68eb6f57fd643b5d8f1d464c9a600b869a8b02e
Osama-Omari <ORZOmari2014@gmail.com>
2025-08-17 16:24:51 +0300

    adding modles

---
+++ SmartAgriGuardAPI/DataAccessLayer/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+
+        public string FullName { get; set; }
+
+        public string username { get; set; }
+
+        public byte[] PasswordHash { get; set; }
+
+        public Guid UserRoleId { get; set; }
+
+        public UserRole UserRole { get; set; }
+
+    }
+}
