eb620450921eb6a53d2a53e00a915f03b8995318
Osama-Omari <ORZOmari2014@gmail.com>
2025-08-27 22:50:36 +0300

    creating the repositories

---
+++ SmartAgriGuardAPI/DataAccessLayer/Repositories/RecommendationRepository.cs
@@ -0,0 +1,13 @@
+﻿using ApplicationLayer.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public class RecommendationRepository : IRecommendationRepository
+    {
+    }
+}
